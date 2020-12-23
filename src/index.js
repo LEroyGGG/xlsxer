11c5ba45cfeeabaccd8359482efafef2abb00b2a
Sergey Kukunin <sergey.kukunin@monterosa.co.uk>
2020-12-24 00:24:05 +0300

    Update root file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const Sheet = require('./libs/Sheet');
+const Styles = require('./libs/Styles');
+
+const Xlsx = require('./libs/Xlsx');
+
+Xlsx.Sheet = Sheet;
+Xlsx.Styles = Styles;
+
+module.exports = Xlsx;
