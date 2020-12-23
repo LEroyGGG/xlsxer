2a7bbf52eb46a0ca5ab5432a0ff70de7dbbe10c7
Sergey Kukunin <sergey.kukunin@monterosa.co.uk>
2020-12-24 00:22:51 +0300

    Update Xlsx lib

--- src/libs/Xlsx/index.js
+++ src/libs/Xlsx/index.js
@@ -1,12 +1,72 @@
-const processData = require('./processors/data');
-const processStyles = require('./processors/styles');
+const Sheet = require('../Sheet');
+const Styles = require('../Styles');
+
+const { isString, isArray } = require('../../utils/types');
 
 class Xlsx {
   constructor(data, styles) {
-    this._data = data;
-    this._styles = styles;
+    this._sheets = [];
+    this._shared = [];
 
-    this.styles = this.prepareStyles();
+    if (isArray(data)) {
+      const sheet = new Sheet('Sheet 1', data, styles || new Styles());
+
+      this._sheets.push(sheet);
+    } else {
+      for (let i = 0, len = arguments.length; i < len; i++) {
+        const sheet = arguments[i];
+
+        if (!(sheet instanceof Sheet)) {
+          throw new Error('Invalid arguments passed into Xlsx constructor');
+        }
+
+        this._sheets.push(sheet);
+      }
+    }
+
+    if (!this._sheets.length) {
+      throw new Error('Xlsx should contain at least one sheet');
+    }
+  }
+
+  collect() {
+    return this._sheets.map(sheet => sheet.collect());
+  }
+
+  combine() {
+    const collected = this.collect();
+
+    this.repack(collected);
+
+    return collected;
+  }
+
+  repack(collected) {
+    for (let i = 0, i_len = collected.length; i < i_len; i++) {
+      const sheet = collected[i];
+
+      for (let j = 0, j_len = sheet.length; j < j_len; j++) {
+        const row = sheet[j];
+
+        for (let k = 0, k_len = row.length; k < k_len; k++) {
+          const cell = row[k];
+
+          const { value } = cell;
+
+          cell.isShared = isString(value) && !/^\d*(\.\d+)?$/.test(value);
+
+          if (cell.isShared) {
+            cell.sharedId = this._shared.length;
+
+            this._shared.push(value);
+          }
+        }
+      }
+    }
+  }
+
+  save(...dest) {
+    dest = path.resolve(...dest);
   }
 }
 
---
+++ src/libs/Xlsx/index.test_.js
@@ -0,0 +1,105 @@
+const Xlsx = require('.');
+
+describe("Xlsx", () => {
+  test("Short creation", () => {
+    const data = [
+      ['Title 1', 'Title 2', 'Title 3', 'Title 4', 'Title 5'],
+      ['Row 1 Col 1', 'Row 1 Col 2', 'Row 1 Col 3', 'Row 1 Col 4', 'Row 1 Col 5'],
+      ['Row 2 Col 1', 'Row 2 Col 2', 'Row 2 Col 3', 'Row 2 Col 4', 'Row 2 Col 5'],
+      ['Row 3 Col 1', 'Row 3 Col 2', 'Row 3 Col 3', 'Row 3 Col 4', 'Row 3 Col 5'],
+      ['Row 4 Col 1', 'Row 4 Col 2', 'Row 4 Col 3', 'Row 4 Col 4', 'Row 4 Col 5'],
+      ['Row 5 Col 1', 'Row 5 Col 2', 'Row 5 Col 3', 'Row 5 Col 4', 'Row 5 Col 5'],
+    ];
+
+    new Xlsx(data).save(__dirname, 'short.debug.xlsx');
+  });
+
+  test("Short creation with styles", () => {
+    const styles = new Xlsx.Styles(__dirname, 'file.css');
+
+    const data = [
+      [
+        { stl: 'TITLE', value: 'Title 1' },
+        { stl: 'TITLE', value: 'Title 2' },
+        { stl: 'TITLE', value: 'Title 3' },
+        { stl: 'TITLE', value: 'Title 4' },
+        { stl: 'TITLE', value: 'Title 5' }
+      ],
+      [
+        { stl: 'VALUE', value: 'Row 1 Col 1' },
+        { stl: 'VALUE', value: 'Row 1 Col 2' },
+        { stl: 'VALUE', value: 'Row 1 Col 3' },
+        { stl: 'VALUE', value: 'Row 1 Col 4' },
+        { stl: 'VALUE', value: 'Row 1 Col 5' }
+      ],
+      [
+        { stl: 'VALUE', value: 'Row 2 Col 1' },
+        { stl: 'VALUE', value: 'Row 2 Col 2' },
+        { stl: 'VALUE', value: 'Row 2 Col 3' },
+        { stl: 'VALUE', value: 'Row 2 Col 4' },
+        { stl: 'VALUE', value: 'Row 2 Col 5' }
+      ]
+    ];
+
+    new Xlsx(data, styles).save(__dirname, 'short-and-styles.debug.xlsx');
+  });
+
+  test("Full step creation with styles", () => {
+    const styles = new Xlsx.Styles(__dirname, 'file.css');
+
+    const data_1 = [
+      [
+        { stl: 'TITLE', value: 'Sheet 1 Title 1' },
+        { stl: 'TITLE', value: 'Sheet 1 Title 2' },
+        { stl: 'TITLE', value: 'Sheet 1 Title 3' },
+        { stl: 'TITLE', value: 'Sheet 1 Title 4' },
+        { stl: 'TITLE', value: 'Sheet 1 Title 5' }
+      ],
+      [
+        { stl: 'VALUE', value: 'Sheet 1 Row 1 Col 1' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 1 Col 2' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 1 Col 3' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 1 Col 4' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 1 Col 5' }
+      ],
+      [
+        { stl: 'VALUE', value: 'Sheet 1 Row 2 Col 1' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 2 Col 2' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 2 Col 3' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 2 Col 4' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 2 Col 5' }
+      ]
+    ];
+    const data_2 = [
+      [
+        { stl: 'TITLE', value: 'Sheet 1 Title 1' },
+        { stl: 'TITLE', value: 'Sheet 1 Title 2' },
+        { stl: 'TITLE', value: 'Sheet 1 Title 3' },
+        { stl: 'TITLE', value: 'Sheet 1 Title 4' },
+        { stl: 'TITLE', value: 'Sheet 1 Title 5' }
+      ],
+      [
+        { stl: 'VALUE', value: 'Sheet 1 Row 1 Col 1' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 1 Col 2' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 1 Col 3' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 1 Col 4' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 1 Col 5' }
+      ],
+      [
+        { stl: 'VALUE', value: 'Sheet 1 Row 2 Col 1' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 2 Col 2' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 2 Col 3' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 2 Col 4' },
+        { stl: 'VALUE', value: 'Sheet 1 Row 2 Col 5' }
+      ]
+    ];
+
+    const styles_1 = styles.clone().readInline('::cols { width: 10, 60, 15, 20, 10, 15, 5 }');
+    const styles_2 = styles.clone().readInline('::cols { width: 60, 25, 10, 10, 15 }');
+
+    const sheet_1 = new Xlsx.Sheet('First sheet', data_1, styles_1);
+    const sheet_2 = new Xlsx.Sheet('Second sheet', data_2, styles_2);
+
+    new Xlsx(sheet_1, sheet_2).meta({ author: 'Me', date: '2020-10-06' }).save(__dirname, 'full.debug.xlsx');
+  });
+});
