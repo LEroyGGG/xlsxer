b66b068ec68a508a31add59158c4cef064bc5d1b
RedMonkey <kukunins@gmail.com>
2021-01-01 03:14:08 +0300

    Add idGenerator to common utils

--- src/utils/common/index.js
+++ src/utils/common/index.js
@@ -1,5 +1,11 @@
 const self = {};
 
+self.idGenerator = prefix => {
+  let last = 0;
+
+  return () => ++last && prefix ? prefix + last : last;
+};
+
 self.pick = function pick(props, src) {
   return props.reduce((r, n) => (n in src && (r[n] = src[n]), r), {});
 };
--- src/utils/common/index.test.js
+++ src/utils/common/index.test.js
@@ -1,6 +1,20 @@
 const self = require('.');
 
 describe("Utils: common", () => {
+  test("idGenerator", () => {
+    let next;
+
+    next = idGenerator();
+
+    expect(next).toEqual(1);
+    expect(next).toEqual(2);
+
+    next = idGenerator('rid');
+
+    expect(next).toEqual('rid1');
+    expect(next).toEqual('rid2');
+  });
+
   test("pick", () => {
     expect(self.pick(['a', 'c', 'd'], { a: 1, b: 2, c: 3 })).toEqual({ a: 1, c: 3 });
   });
