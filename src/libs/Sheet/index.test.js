db38aee90e17860f488485e590e2538e6d759306
Sergey Kukunin <sergey.kukunin@monterosa.co.uk>
2021-10-09 02:28:40 +0300

    Update Sheet test file

--- src/libs/Sheet/index.test.js
+++ src/libs/Sheet/index.test.js
@@ -1,7 +1,5 @@
 const Sheet = require('.');
 
-const Styles = require('../Styles');
-
 describe("Sheet", () => {
   test("Simple create", () => {
     const data = [
@@ -20,62 +18,420 @@ describe("Sheet", () => {
     ];
 
     const result = [
-      [
-        { "value": "Title 1" },
-        { "value": "Title 2" },
-        { "value": "Title 3", "rowSpan": 3, "colSpan": 3 },
-        { "value": null, "lockedBy": [2, 0] },
-        { "value": null, "lockedBy": [2, 0] },
-        { "value": "Title 4" },
-        { "value": "Title 5" }
-      ],
-      [
-        { "value": "Row 1 Col 1" },
-        { "value": "Row 1 Col 2" },
-        { "value": null, "lockedBy": [2, 0] },
-        { "value": null, "lockedBy": [2, 0] },
-        { "value": null, "lockedBy": [2, 0] },
-        { "value": "Row 1 Col 3" },
-        { "value": "Row 1 Col 4" },
-        { "value": "Row 1 Col 5" }
-      ],
-      [
-        { "value": "Row 2 Col 1" },
-        { "value": "Row 2 Col 2" },
-        { "value": null, "lockedBy": [2, 0] },
-        { "value": null, "lockedBy": [2, 0] },
-        { "value": null, "lockedBy": [2, 0] },
-        { "value": "Row 2 Col 3" },
-        { "value": "Row 2 Col 4" },
-        { "value": "Row 2 Col 5" }
-      ],
-      [
-        { "value": "Row 3 Col 1" },
-        { "value": "Row 3 Col 2" },
-        { "value": "Row 3 Col 3" },
-        { "value": "Row 3 Col 4" },
-        { "value": "Row 3 Col 5" }
-      ],
-      [
-        { "value": "Row 4 Col 1" },
-        { "value": "Row 4 Col 2" },
-        { "value": "Row 4 Col 3" },
-        { "value": "Row 4 Col 4" },
-        { "value": "Row 4 Col 5" }
-      ],
-      [
-        { "value": "Row 5 Col 1" },
-        { "value": "Row 5 Col 2" },
-        { "value": "Row 5 Col 3" },
-        { "value": "Row 5 Col 4" },
-        { "value": "Row 5 Col 5" }
-      ]
-    ];
+  {
+    "cells": [
+      {
+        "_index": [
+          0,
+          0
+        ],
+        "_value": "Title 1",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          0,
+          1
+        ],
+        "_value": "Title 2",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          0,
+          2
+        ],
+        "_value": "Title 3",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          0,
+          3
+        ],
+        "_value": null,
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          0,
+          4
+        ],
+        "_value": null,
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          0,
+          5
+        ],
+        "_value": "Title 4",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          0,
+          6
+        ],
+        "_value": "Title 5",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      }
+    ],
+    "spans": "1:7"
+  },
+  {
+    "cells": [
+      {
+        "_index": [
+          1,
+          0
+        ],
+        "_value": "Row 1 Col 1",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          1,
+          1
+        ],
+        "_value": "Row 1 Col 2",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          1,
+          2
+        ],
+        "_value": null,
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          1,
+          3
+        ],
+        "_value": null,
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          1,
+          4
+        ],
+        "_value": null,
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          1,
+          5
+        ],
+        "_value": "Row 1 Col 3",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          1,
+          6
+        ],
+        "_value": "Row 1 Col 4",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          1,
+          7
+        ],
+        "_value": "Row 1 Col 5",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      }
+    ],
+    "spans": "1:8"
+  },
+  {
+    "cells": [
+      {
+        "_index": [
+          2,
+          0
+        ],
+        "_value": "Row 2 Col 1",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          2,
+          1
+        ],
+        "_value": "Row 2 Col 2",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          2,
+          2
+        ],
+        "_value": null,
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          2,
+          3
+        ],
+        "_value": null,
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          2,
+          4
+        ],
+        "_value": null,
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          2,
+          5
+        ],
+        "_value": "Row 2 Col 3",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          2,
+          6
+        ],
+        "_value": "Row 2 Col 4",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          2,
+          7
+        ],
+        "_value": "Row 2 Col 5",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      }
+    ],
+    "spans": "1:8"
+  },
+  {
+    "cells": [
+      {
+        "_index": [
+          3,
+          0
+        ],
+        "_value": "Row 3 Col 1",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          3,
+          1
+        ],
+        "_value": "Row 3 Col 2",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          3,
+          2
+        ],
+        "_value": "Row 3 Col 3",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          3,
+          3
+        ],
+        "_value": "Row 3 Col 4",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          3,
+          4
+        ],
+        "_value": "Row 3 Col 5",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      }
+    ],
+    "spans": "1:5"
+  },
+  {
+    "cells": [
+      {
+        "_index": [
+          4,
+          0
+        ],
+        "_value": "Row 4 Col 1",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          4,
+          1
+        ],
+        "_value": "Row 4 Col 2",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          4,
+          2
+        ],
+        "_value": "Row 4 Col 3",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          4,
+          3
+        ],
+        "_value": "Row 4 Col 4",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          4,
+          4
+        ],
+        "_value": "Row 4 Col 5",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      }
+    ],
+    "spans": "1:5"
+  },
+  {
+    "cells": [
+      {
+        "_index": [
+          5,
+          0
+        ],
+        "_value": "Row 5 Col 1",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          5,
+          1
+        ],
+        "_value": "Row 5 Col 2",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          5,
+          2
+        ],
+        "_value": "Row 5 Col 3",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          5,
+          3
+        ],
+        "_value": "Row 5 Col 4",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      },
+      {
+        "_index": [
+          5,
+          4
+        ],
+        "_value": "Row 5 Col 5",
+        "_styles": null,
+        "_isShared": false,
+        "_sharedId": null
+      }
+    ],
+    "spans": "1:5"
+  }
+];
 
-    const styles = new Styles();
+    const sheet = new Sheet('Test tab', data);
 
-    const sheet = new Sheet('Test tab', data, styles);
-
-    expect(sheet.collect()).toEqual(result);
+    expect(sheet.getData()).toEqual(result);
   });
 });
